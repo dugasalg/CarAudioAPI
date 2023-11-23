fb7026122ae018bc0d8727188f820eb576100962
dugasalg <73446031+dugasalg@users.noreply.github.com>
2023-11-22 19:00:23 -0600

    login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,85 +1,65 @@
 const jwt = require('jsonwebtoken');
-const {
-    config
-} = require('../config/config');
-const { head } = require('../routes');
+const { config } = require('../config/config');
 
+async function firmarjwt(req,res){
 
-async function firmaJwt(req, res) {
-    try {
+    try{
         const nuevoToken = await jwt.sign(
-            { email: "salgado@gmail.com" },
+            {email: "dugatabacc@gmail.com"},
             config.auth.secretKey,
-            { algorithm: 'HS256' }
-        );
+            {algorithm:'HS256'}
+            );
 
-        res.status(200).json({
-            message: "Token creado",
-            jwt: nuevoToken
-        });
-    } catch (err) {
+            res.status(200).json({
+                message: "OK",
+                jwt: nuevoToken
+            })
+ 
+    }catch(err){
         res.status(500).json({
             message: "Error al crear el token jwt"
-        });
+        })
     }
-
-}
-async function verifyJwt(req, res,next) {
-    const headerToken = req.headers.authorization;
-    let authToken;
-
-    if (headerToken && headerToken.length){
-        const tokenParts = headerToken.split(' ');
-        if(tokenParts.length == 2){
-            authToken = tokenParts[1];
-        }
-        try{
-            await jwt.verify(authToken,config.auth.secretKey);
-            next();
-        }catch(err){
-            console.error("INVALID TOKEN");
-        }
-    } else{
-        res.status(500).json({
-            
-        });
-    }
-    console.log(headerToken);
-    res.status(200);
-}
-
-
-// Controlador para iniciar sesión y obtener un token JWT
-async function iniciarSesion(req, res) {
-  const { usuario, contraseña } = req.body;
-  try {
-    // Realiza la lógica de verificación de credenciales en la base de datos
-    const usuarioValido = await User.findOne({ username: usuario, password: contraseña });
     
-    if (usuarioValido) {
-      // Genera un token JWT
-      const token = jwt.sign({ usuario: usuarioValido.username }, config.auth.secretKey, { algorithm: 'HS256' });
-      res.status(200).json({ mensaje: 'Inicio de sesión exitoso', jwt: token });
-    } else {
-      res.status(401).json({ mensaje: 'Nombre de usuario o contraseña incorrectos' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Error al iniciar sesión' });
-  }
 }
-
 async function servicioPrueba(req,res){
     console.log("Entro a funcion");
     res.status(200).json(
-        {message:"CORRECTO"}
+        {message:"holaa"}
 
     );
+
+}
+
+async function verifyjwt(req, res, next) {
+    console.log("verifyjwt");
+    
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    let authToken;
+
+    if (authHeader && authHeader.length) {
+        const tokenParts = authHeader.split(' ');
+        if (tokenParts.length === 2) {
+            authToken = tokenParts[1];
+        }
+    }
+
+    try {
+        await jwt.verify(authToken, config.auth.secretKey);
+        console.log("Paso comprobacion");
+        next();
+    } catch (err) {
+        console.log('Error de verificación:', err.message); // imprime el mensaje del error
+        res.status(400).json({ message: "No autorizado" });
+    }
+    
+
 }
 
 
 module.exports = {
-    firmaJwt,
-    verifyJwt,
-    iniciarSesion,
-    servicioPrueba
+    firmarjwt,verifyjwt,servicioPrueba
+
+
 }
--- routes/users.js
+++ routes/users.js
@@ -2,28 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 const {
-  registrarUsuario, iniciarSesion, getPedidosByUsuario, actualizarUsuario, eliminarUsuarioLogico
+  registrarUsuario, loginUsuario
 } = require('../controllers/user.controller.js');
-
- 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/iniciar-sesion', iniciarSesion);
-router.post('/registrar', registrarUsuario);
-// Ruta para obtener los pedidos de un usuario
-router.get('/pedidos/:usuarioId', getPedidosByUsuario);
-// Ruta para actualizar la información del usuario
-router.put('/usuario/:id', actualizarUsuario);
-// Ruta para eliminar lógicamente la cuenta del usuario
-router.delete('/usuario/:id', eliminarUsuarioLogico);
+router.post('/registrar',registrarUsuario);
+router.post('/login',loginUsuario)
 
-
-
-
+module.exports = router;
 module.exports = router;
 
 
